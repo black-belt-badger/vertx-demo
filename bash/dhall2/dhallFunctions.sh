a4fecc64cbad508478f470913176dc27b9cbaa71
Marek Dudek <marek.dudek@gmail.com>
2025-07-14 01:46:25 +0200

    generating help

--- bash/dhall2/dhallFunctions.sh
+++ bash/dhall2/dhallFunctions.sh
@@ -4,41 +4,51 @@ FILE=$(readlink -f "$0")
 DIR=$(dirname "$FILE")
 set -x
 
-# dhall-json
+DHALL_JSON=dhallhaskell/dhall-json
 
 function dhallToJson() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
-  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    dhallhaskell/dhall-json dhall-to-json --file "$IN" --output "$OUT"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw $DHALL_JSON dhall-to-json --file "$IN" --output "$OUT"
+}
+
+function dhallToJsonHelp() {
+  docker run -i --rm $DHALL_JSON dhall-to-json -h > ./docs/dhall-to-json.txt
 }
 
 function dhallToYaml() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
-  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    dhallhaskell/dhall-json dhall-to-yaml --file "$IN" --output "$OUT"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw $DHALL_JSON dhall-to-yaml --file "$IN" --output "$OUT"
+}
+
+function dhallToYamlHelp() {
+  docker run -i --rm $DHALL_JSON dhall-to-yaml -h > ./docs/dhall-to-yaml.txt
 }
 
 function jsonToDhall() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
   docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    dhallhaskell/dhall-json json-to-dhall --file "$IN" --output "$OUT"
+    $DHALL_JSON json-to-dhall --file "$IN" --output "$OUT"
 }
 
-# dhall-yaml
+DHALL_YAML=dhallhaskell/dhall-yaml
 
 function dhallToYaml2ng() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
   docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    dhallhaskell/dhall-yaml dhall-to-yaml-ng --file "$IN" --output "$OUT"
+    $DHALL_YAML dhall-to-yaml-ng --file "$IN" --output "$OUT"
 }
 
 function yamlToDhall2() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
   docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    dhallhaskell/dhall-yaml yaml-to-dhall --file "$IN" --output "$OUT"
+    $DHALL_YAML yaml-to-dhall --file "$IN" --output "$OUT"
 }
+
+DHALL=dhallhaskell/dhall
+
+
--- bash/dhall2/testFunctions.sh
+++ bash/dhall2/testFunctions.sh
@@ -9,3 +9,6 @@ jsonToDhall "config.json" "config.dhall"
 
 dhallToYaml2ng "compose-dev.dhall" "compose-dev-2ng.yaml"
 yamlToDhall2 "compose-dev.yaml" "compose-dev-2.dhall"
+
+dhallToJsonHelp
+dhallToYamlHelp
