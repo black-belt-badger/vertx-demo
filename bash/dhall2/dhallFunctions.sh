8ff30b3346a547dd0d53a6ec4ac3fecc022e6bcf
Marek Dudek <marek.dudek@gmail.com>
2025-07-14 00:56:26 +0200

    dhall -> yaml function

--- bash/dhall2/dhallFunctions.sh
+++ bash/dhall2/dhallFunctions.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 FILE=$(readlink -f "$0")
-DIR=$(dirname "${FILE}")
+DIR=$(dirname "$FILE")
 set -x
 
 function yamlToDhall() {
-  local INPUT=/input/"$1"
-  local OUTPUT=/output/"$2"
-  docker run -i --rm -v "$DIR"/input/:/input:ro -v "$DIR"/output/:/output:rw \
-    dhallhaskell/dhall-yaml yaml-to-dhall --file "$INPUT" --output "$OUTPUT"
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
+    dhallhaskell/dhall-yaml yaml-to-dhall --file "$IN" --output "$OUT"
 }
 
 function dhallToYaml() {
-  local INPUT=/input/"$1"
-    local OUTPUT=/output/"$2"
-    docker run -i --rm -v "$DIR"/input/:/input:ro -v "$DIR"/output/:/output:rw \
-      dhallhaskell/dhall-yaml dhall-to-yaml-ng --file "$INPUT" --output "$OUTPUT"
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
+    dhallhaskell/dhall-yaml dhall-to-yaml-ng --file "$IN" --output "$OUT"
 }
