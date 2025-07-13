d9e010f9185824c5531d7fd48c23d434f57fd85a
Marek Dudek <marek.dudek@gmail.com>
2025-07-14 01:56:33 +0200

    generating help

--- bash/dhall2/dhallFunctions.sh
+++ bash/dhall2/dhallFunctions.sh
@@ -29,8 +29,11 @@ function dhallToYamlHelp() {
 function jsonToDhall() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
-  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    $DHALL_JSON json-to-dhall --file "$IN" --output "$OUT"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw $DHALL_JSON json-to-dhall --file "$IN" --output "$OUT"
+}
+
+function jsonToDhallHelp() {
+  docker run -i --rm $DHALL_JSON json-to-dhall -h > ./docs/json-to-dhall.txt
 }
 
 DHALL_YAML=dhallhaskell/dhall-yaml
@@ -38,15 +41,21 @@ DHALL_YAML=dhallhaskell/dhall-yaml
 function dhallToYaml2ng() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
-  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    $DHALL_YAML dhall-to-yaml-ng --file "$IN" --output "$OUT"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw $DHALL_YAML dhall-to-yaml-ng --file "$IN" --output "$OUT"
+}
+
+function dhallToYaml2ngHelp() {
+  docker run -i --rm $DHALL_YAML dhall-to-yaml-ng -h > ./docs/dhall-to-yaml-ng.txt
 }
 
 function yamlToDhall2() {
   local IN=/in/"$1"
   local OUT=/out/"$2"
-  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw \
-    $DHALL_YAML yaml-to-dhall --file "$IN" --output "$OUT"
+  docker run -i --rm -v "$DIR"/in/:/in:ro -v "$DIR"/out/:/out:rw $DHALL_YAML yaml-to-dhall --file "$IN" --output "$OUT"
+}
+
+function yamlToDhall2Help() {
+  docker run -i --rm $DHALL_YAML yaml-to-dhall -h > ./docs/yaml-to-dhall.txt
 }
 
 DHALL=dhallhaskell/dhall
--- bash/dhall2/testFunctions.sh
+++ bash/dhall2/testFunctions.sh
@@ -6,9 +6,11 @@ set -x
 dhallToJson "config.dhall" "config.json"
 dhallToYaml "compose-dev.dhall" "compose-dev.yaml"
 jsonToDhall "config.json" "config.dhall"
-
 dhallToYaml2ng "compose-dev.dhall" "compose-dev-2ng.yaml"
 yamlToDhall2 "compose-dev.yaml" "compose-dev-2.dhall"
 
 dhallToJsonHelp
 dhallToYamlHelp
+jsonToDhallHelp
+dhallToYaml2ngHelp
+yamlToDhall2Help
