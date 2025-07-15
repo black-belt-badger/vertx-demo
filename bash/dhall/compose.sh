ba9bc5453f274a3bb347165ad7203c687edcbb2a
Marek Dudek <marek.dudek@gmail.com>
2025-07-15 09:10:25 +0200

    formatted more files

---
+++ bash/dhall/compose.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+source ./bin/conversion-functions.sh
+set -x
+
+rm -fr ./data/compose/out/*
+
+dhallFormat compose.dhall ./data/compose
+dhallToYaml2ngExtract compose.dhall compose-dev.yaml  ./data/compose dev
+dhallToYaml2ngExtract compose.dhall compose-prod.yaml ./data/compose prod
+
+mkdir -p ./data/compose/out/configs/dev
+dhallFormat conf.dhall ./data/compose
+dhallToJson conf.dhall configs/dev/conf.json ./data/compose
--- bash/dhall/config.sh
+++ bash/dhall/config.sh
@@ -4,4 +4,9 @@ source ./bin/conversion-functions.sh
 set -x
 
 rm -fr ./data/config/out/
+
+dhallFormat config.dhall ./data/config
 dhallToJson config.dhall config.json ./data/config
+
+dhallFormat configs.dhall ./data/config
+dhallToDirectoryTree configs.dhall config ./data/config
