e803f9844f70ea252cf47a1e2327af3f0b777ad5
Marek Dudek <marek.dudek@gmail.com>
2025-07-15 07:25:29 +0200

    forgotten functions

---
+++ bash/dhall/bin/conversion-functions.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+function dhallToJson() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall-json \
+    dhall-to-json \
+    --file "$IN" \
+    --output "$OUT"
+}
+
+function dhallToJsonToStdout() {
+  local IN=/in/"$1"
+  local DATA="$2"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    dhallhaskell/dhall-json \
+    dhall-to-json \
+    <<< "$IN"
+}
+
+function dhallToYaml() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall-json \
+    dhall-to-yaml \
+    --file "$IN" \
+    --output "$OUT"
+}
+
+function jsonToDhall() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall-json \
+    json-to-dhall \
+    --file "$IN" \
+    --output "$OUT"
+}
+
+function dhallToYaml2ng() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall-yaml \
+    dhall-to-yaml-ng \
+    --file "$IN" \
+    --output "$OUT"
+}
+
+function yamlToDhall2() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall-yaml \
+    yaml-to-dhall \
+    --file "$IN" \
+    --output "$OUT"
+}
+
+function dhallToDirectoryTree() {
+  local IN=/in/"$1"
+  local OUT=/out/"$2"
+  local DATA="$3"
+  docker run -i --rm \
+    -v "$DATA"/in/:/in:ro \
+    -v "$DATA"/out/:/out:rw \
+    dhallhaskell/dhall \
+    dhall to-directory-tree \
+    --file "$IN" \
+    --output "$OUT"
+}
+
