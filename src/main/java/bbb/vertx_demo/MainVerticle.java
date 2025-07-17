7cf799bc75e7abbb1895b1135a0c480bc18ee1d5
Marek Dudek <marek.dudek@gmail.com>
2025-07-17 23:03:08 +0200

    refactoring done

--- src/main/java/bbb/vertx_demo/MainVerticle.java
+++ src/main/java/bbb/vertx_demo/MainVerticle.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static bbb.vertx_demo.main.CommandRegistrator.registerCommand;
-import static bbb.vertx_demo.main.ConfigRetrieverCreator.createConfigRetriever;
+import static bbb.vertx_demo.main.ConfigRetrieverCreator.retrieveAndMerge;
 import static bbb.vertx_demo.main.HttpServerStarter.startHttpServer;
 import static bbb.vertx_demo.main.MBeanRegistrator.registerMBean;
 import static bbb.vertx_demo.main.PostgresConnectionStarter.connectToPostgres;
@@ -20,59 +20,36 @@ public final class MainVerticle extends VerticleBase {
   @Override
   public Future<?> start() {
     var starting = config();
-    log.info("Start config: {}", starting.encodePrettily());
-    var configServerConfig = starting.getJsonObject("config-server", new JsonObject());
-    var retriever = createConfigRetriever(vertx, configServerConfig);
-    return retriever
-      .getConfig()
-      .map(retrieved -> {
-          log.info("Retrieved config: {}", retrieved.encodePrettily());
-          var merged = retrieved.mergeIn(starting);
-          log.info("Merged config: {}", merged.encodePrettily());
-          var configServerVersion =
-            new AtomicReference<>(
-              merged
-                .getJsonObject("config-server", new JsonObject())
-                .getString("version", "compiled default value")
-            );
-          retriever
-            .listen(change -> {
-                var previous = change.getPreviousConfiguration();
-                var next = change.getNewConfiguration();
-                log.info("Config changed from {} to {}", previous.encodePrettily(), next.encodePrettily());
-                var newConfigServerVersion =
-                  next
-                    .getJsonObject("config-server", new JsonObject())
-                    .getString("version");
-                if (newConfigServerVersion != null)
-                  configServerVersion.set(newConfigServerVersion);
-              }
-            );
-          var httpHost = merged.getString("http.host", "0.0.0.0");
-          int httpPort = merged.getInteger("http.port", 8080);
-          return
-            startHttpServer(vertx, configServerVersion, httpPort, httpHost)
-              .flatMap(ignored -> {
-                  var host =
-                    merged.getString("telnet.host", "0.0.0.0");
-                  int port =
-                    merged.getInteger("telnet.port", 5000);
-                  return deployShell(vertx, host, port);
-                }
-              )
-              .flatMap(ignored ->
-                registerCommand(vertx, merged)
-              )
-              .flatMap(ignored ->
-                registerMBean()
-              )
-              .andThen(ignored -> {
-                  var config =
-                    merged.getJsonObject("postgres", new JsonObject());
-                  connectToPostgres(vertx, config);
-                }
-              );
-        }
-      );
+    log.info("Starting config: {}", starting.encodePrettily());
+    var configServerVersion = new AtomicReference<String>();
+    return
+      retrieveAndMerge(vertx, starting, configServerVersion)
+        .map(merged -> {
+            var httpHost = merged.getString("http.host", "0.0.0.0");
+            int httpPort = merged.getInteger("http.port", 8080);
+            return
+              startHttpServer(vertx, configServerVersion, httpPort, httpHost)
+                .flatMap(ignored -> {
+                    var host =
+                      merged.getString("telnet.host", "0.0.0.0");
+                    int port =
+                      merged.getInteger("telnet.port", 5000);
+                    return deployShell(vertx, host, port);
+                  }
+                )
+                .flatMap(ignored ->
+                  registerCommand(vertx, merged)
+                )
+                .flatMap(ignored ->
+                  registerMBean()
+                )
+                .andThen(ignored -> {
+                    var config =
+                      merged.getJsonObject("postgres", new JsonObject());
+                    connectToPostgres(vertx, config);
+                  }
+                );
+          }
+        );
   }
 }
--- src/main/java/bbb/vertx_demo/main/ConfigRetrieverCreator.java
+++ src/main/java/bbb/vertx_demo/main/ConfigRetrieverCreator.java
@@ -3,22 +3,27 @@ package bbb.vertx_demo.main;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 public enum ConfigRetrieverCreator {
 
   ;
 
-  public static ConfigRetriever createConfigRetriever
+  public static Future<JsonObject> retrieveAndMerge
     (
       Vertx vertx,
-      JsonObject configServer
+      JsonObject starting,
+      AtomicReference<String> configServerVersion
     ) {
+    var configServer = starting
+      .getJsonObject("config-server", new JsonObject());
     var host = configServer.getString("host", "localhost");
     log.info("Config server host: {}", host);
     var port = configServer.getInteger("port", 8887);
@@ -29,39 +34,69 @@ public enum ConfigRetrieverCreator {
     log.info("Config server scan period string: {}", scanPeriodString);
     var scanPeriod = Duration.parse(scanPeriodString);
     log.info("Config server scan period: {}", scanPeriod);
-    return ConfigRetriever
-      .create(
-        vertx,
-        new ConfigRetrieverOptions()
-          .setScanPeriod(
-            scanPeriod.toMillis()
-          )
-          .addStore(
-            new ConfigStoreOptions()
-              .setType("http")
-              .setOptional(true)
-              .setConfig(
-                new JsonObject()
-                  .put("host", host)
-                  .put("port", port)
-                  .put("path", path)
-                  .put("headers",
-                    new JsonObject()
-                      .put("Accept", "application/json")
-                  )
-              )
-          )
-      )
-      .setBeforeScanHandler(ignored -> {
-          if (log.isDebugEnabled())
-            log.debug("About to scan config");
-        }
-      )
-      .setConfigurationProcessor(config -> {
-          if (log.isDebugEnabled())
-            log.debug("Processing config: {}", config.encodePrettily());
-          return config;
-        }
-      );
+    var retriever =
+      ConfigRetriever
+        .create(
+          vertx,
+          new ConfigRetrieverOptions()
+            .setScanPeriod(
+              scanPeriod.toMillis()
+            )
+            .addStore(
+              new ConfigStoreOptions()
+                .setType("http")
+                .setOptional(true)
+                .setConfig(
+                  new JsonObject()
+                    .put("host", host)
+                    .put("port", port)
+                    .put("path", path)
+                    .put("headers",
+                      new JsonObject()
+                        .put("Accept", "application/json")
+                    )
+                )
+            )
+        );
+    return
+      retriever
+        .setBeforeScanHandler(ignored -> {
+            if (log.isDebugEnabled())
+              log.debug("About to scan config");
+          }
+        )
+        .setConfigurationProcessor(config -> {
+            if (log.isDebugEnabled())
+              log.debug("Processing config: {}", config.encodePrettily());
+            return config;
+          }
+        )
+        .getConfig()
+        .map(retrieved -> {
+            log.info("Retrieved config: {}", retrieved.encodePrettily());
+            var merged =
+              retrieved.mergeIn(starting);
+            log.info("Merged config: {}", merged.encodePrettily());
+            var configVersion =
+              merged
+                .getJsonObject("config-server", new JsonObject())
+                .getString("version", "compiled default value");
+            configServerVersion.set(configVersion);
+            retriever
+              .listen(change -> {
+                  var previous = change.getPreviousConfiguration();
+                  var next = change.getNewConfiguration();
+                  log.info("Config changed from {} to {}", previous.encodePrettily(), next.encodePrettily());
+                  var newConfigVersion =
+                    next
+                      .getJsonObject("config-server", new JsonObject())
+                      .getString("version");
+                  if (newConfigVersion != null)
+                    configServerVersion.set(newConfigVersion);
+                }
+              );
+            return merged;
+          }
+        );
   }
 }
