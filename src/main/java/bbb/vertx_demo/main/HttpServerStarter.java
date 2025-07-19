f43d5328c07e45e7b26b3119c81cae454e309f03
Marek Dudek <marek.dudek@gmail.com>
2025-07-19 02:06:59 +0200

    health check for the app

--- src/main/java/bbb/vertx_demo/main/HttpServerStarter.java
+++ src/main/java/bbb/vertx_demo/main/HttpServerStarter.java
@@ -3,10 +3,15 @@ package bbb.vertx_demo.main;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static io.vertx.ext.healthchecks.Status.KO;
+import static io.vertx.ext.healthchecks.Status.OK;
+import static io.vertx.ext.web.healthchecks.HealthCheckHandler.create;
 import static java.lang.String.format;
 import static java.lang.System.getenv;
 import static java.util.Optional.ofNullable;
@@ -25,19 +30,27 @@ public enum HttpServerStarter {
       int port,
       String host
     ) {
+     var router = Router.router(vertx);
+     router.get("/health").handler(
+    create(vertx).register(
+                      "server-online", promise -> {
+                          var result = new Random().nextBoolean() ? OK() : KO();
+                          promise.complete(result);
+                      }
+              )
+      );
+     router.get("/").handler(
+              context ->
+                      context.response()
+                              .putHeader("content-type", "text/plain")
+                              .end(
+                                      format("Hello from Vert.x Demo, version '%s', config '%s'!", VERSION, configServerVersionRef.get())
+                              )
+
+    );
     return vertx
       .createHttpServer()
-      .requestHandler(request -> {
-          if (log.isTraceEnabled())
-            log.trace("Received request: {}", request.uri());
-          request
-            .response()
-            .putHeader("content-type", "text/plain")
-            .end(
-              format("Hello from Vert.x Demo, version '%s', config '%s'!", VERSION, configServerVersionRef.get())
-            );
-        }
-      )
+      .requestHandler(router)
       .listen(port, host)
       .onSuccess(server ->
         log.info("HTTP server started on internal {}:{}", host, port))
