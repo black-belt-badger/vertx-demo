5d2f87e074172ef2b75a424d9b3bc586bcc13179
Marek Dudek <marek.dudek@gmail.com>
2025-07-21 01:24:05 +0200

    simplest example

--- src/main/java/bbb/vertx_demo/main/HttpServerStarter.java
+++ src/main/java/bbb/vertx_demo/main/HttpServerStarter.java
@@ -4,14 +4,15 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.healthchecks.HealthCheckHandler;
+import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 import static io.vertx.ext.healthchecks.Status.KO;
-import static java.lang.String.format;
 import static java.lang.System.getenv;
 import static java.util.Optional.ofNullable;
 
@@ -37,14 +38,28 @@ public enum HttpServerStarter {
       .handler(
         checks.register(WEB_SERVER_ONLINE, Promise::succeed)
       );
+    var engine = ThymeleafTemplateEngine.create(vertx);
     router.get("/")
-      .handler(context ->
-        context.response()
-          .putHeader("content-type", "text/plain")
-          .end(
-            format("Hello from Vert.x Demo, version '%s', config '%s'!", VERSION, configServerVersionRef.get())
-          )
+      .handler(context -> {
+          engine
+            .render(
+              new JsonObject()
+                .put("foo", "Foo")
+                .put("bar", "Bar")
+                .put("version", configServerVersionRef.get()
+                ),
+              "thymeleaf/example-template.html"
+            )
+            .onFailure(throwable -> log.error("error rendering template", throwable))
+            .onSuccess(buffer -> {
+                context.response()
+                  .putHeader("content-type", "text/plain")
+                  .end(buffer);
+              }
+            );
+        }
       );
+
     return vertx
       .createHttpServer()
       .requestHandler(router)
