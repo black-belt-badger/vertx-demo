23a205b2cac5a6ba811d067543b65a82ba7a424a
Marek Dudek <marek.dudek@gmail.com>
2025-07-27 12:47:53 +0200

    moved assets to separate files

---
+++ src/main/resources/webroot/static/beautify-ipo-table.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('td.date').forEach(td => {
+    const date = new Date(td.textContent);
+    td.textContent = date.toLocaleDateString('en-US', {
+        year: 'numeric', month: 'short', day: 'numeric'
+      }
+    );
+  }
+);
+document.querySelectorAll('td.shares').forEach(td => {
+    td.textContent = Number(td.textContent).toLocaleString();
+  }
+);
+document.querySelectorAll('td.price').forEach(td => {
+    td.textContent = `$${Number(td.textContent).toFixed(2)}`;
+  }
+);
