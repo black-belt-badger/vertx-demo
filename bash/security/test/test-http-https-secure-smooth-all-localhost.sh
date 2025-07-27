bf9ea08fdb58235498786a1ff6e8c19f8e4f2868
Marek Dudek <marek.dudek@gmail.com>
2025-07-27 15:02:30 +0200

    1.0.27

--- bash/security/test/test-http-https-secure-smooth-all-localhost.sh
+++ bash/security/test/test-http-https-secure-smooth-all-localhost.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-curl -k http://localhost:8080
-curl -k --cert ../smooth-server/server.cert.pem --key ../smooth-server/server.key.pem https://localhost:8443
-curl --cacert ../smooth-ca/ca.cert.pem --cert ../smooth-server/server.cert.pem --key ../smooth-server/server.key.pem https://localhost:8443
+curl -f --output /dev/null -k http://localhost:8080
+curl -f --output /dev/null -k --cert ../smooth-server/server.cert.pem --key ../smooth-server/server.key.pem https://localhost:8443
+curl -f --output /dev/null --cacert ../smooth-ca/ca.cert.pem --cert ../smooth-server/server.cert.pem --key ../smooth-server/server.key.pem https://localhost:8443
