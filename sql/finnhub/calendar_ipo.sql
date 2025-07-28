2c69a14f2048cbb0b0eed4830a6ead655bf866ef
Marek Dudek <marek.dudek@gmail.com>
2025-07-28 19:39:47 +0200

    finnhub schema scripts

---
+++ sql/finnhub/calendar_ipo.sql
@@ -0,0 +1,20 @@
+-- Table: finnhub.calendar_ipo
+
+-- DROP TABLE IF EXISTS finnhub.calendar_ipo;
+
+CREATE TABLE IF NOT EXISTS finnhub.calendar_ipo
+(
+  date date,
+  exchange text COLLATE pg_catalog."default",
+  name text COLLATE pg_catalog."default",
+  number_of_shares integer,
+  price text COLLATE pg_catalog."default",
+  status text COLLATE pg_catalog."default",
+  symbol text COLLATE pg_catalog."default",
+  total_shares_value bigint
+)
+
+  TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS finnhub.calendar_ipo
+  OWNER to vertx_demo_dev_user;
