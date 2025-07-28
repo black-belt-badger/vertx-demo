0da74b119f5b260fffb6344bdfd6d9cf2fa5aa11
Marek Dudek <marek.dudek@gmail.com>
2025-07-28 15:36:50 +0200

    script fixed

--- dhall/calendar-ipo.sh
+++ dhall/calendar-ipo.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 set -x
 
-jq '.ipoCalendar // []' ./input/calendar-ipo.json > ./output/calendar-ipo-array.json
-json-to-dhall '(List ./input/IPO-type.dhall)' < ./output/calendar-ipo-array.json > ./output/calendar-ipo.dhall
+INPUT_JSON=./input/calendar-ipo.json
+INTERMEDIATE_JSON=./output/calendar-ipo-array.json
+INTERMEDIATE_DHALL=./output/calendar-ipo.dhall
+OUTPUT_SQL=./output/calendar-ipo.sql
 
-calendar-ipo-dhall-to-sql ./output/calendar-ipo.dhall ./output/calendar-ipo.sql
-
+jq '.ipoCalendar // []' $INPUT_JSON > $INTERMEDIATE_JSON
+json-to-dhall '(List ./input/IPO-type.dhall)' < $INTERMEDIATE_JSON > $INTERMEDIATE_DHALL
+calendar-ipo-dhall-to-sql $INTERMEDIATE_DHALL $OUTPUT_SQL
 export PGPASSWORD=vertx_demo_dev_password
-psql -U vertx_demo_dev_user -d vertx_demo_dev_database -h localhost -f /tmp/insert_ipos.sql
+psql -U vertx_demo_dev_user -d vertx_demo_dev_database -h localhost -f $OUTPUT_SQL
