7f325a7d6d9a672bba5b6e824f05e39362ddc8f7
Marek Dudek <marek.dudek@gmail.com>
2025-07-29 17:42:58 +0200

    earnings calendar

--- bash/apis/finnhub/Earnings-Calendar.sh
+++ bash/apis/finnhub/Earnings-Calendar.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 source ./finnhub.sh
 set -x
 
-REQUEST="/calendar/earnings?symbol=AAPL"
+REQUEST="/calendar/earnings?from=2025-07-29&to=2025-08-31"
 
 curl -H "${AUTH}" "${BASE_URL}/${REQUEST}" > output/Earnings-Calendar-AAPL.json
+
+#GET https://finnhub.io/api/v1/calendar/earnings?from=2025-07-29&to=2025-08-31
