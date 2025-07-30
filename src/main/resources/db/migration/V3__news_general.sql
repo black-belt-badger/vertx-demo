97f5a53d621f1183e1278709e36a04c82dfad96b
Marek Dudek <marek.dudek@gmail.com>
2025-07-30 17:02:54 +0200

    entire DDL

---
+++ src/main/resources/db/migration/V3__news_general.sql
@@ -0,0 +1,42 @@
+-- Table: finnhub.news_general
+
+-- DROP TABLE IF EXISTS finnhub.news_general;
+
+CREATE TABLE IF NOT EXISTS finnhub.news_general
+(
+  category text COLLATE pg_catalog."default",
+  datetime integer,
+  headline text COLLATE pg_catalog."default",
+  id       integer,
+  image    text COLLATE pg_catalog."default",
+  related  text COLLATE pg_catalog."default",
+  source   text COLLATE pg_catalog."default",
+  summary  text COLLATE pg_catalog."default",
+  url      text COLLATE pg_catalog."default"
+) TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS finnhub.news_general
+  OWNER to vertx_demo_dev_user;
+
+-- indexes
+
+CREATE INDEX idx_news_general_datetime_desc ON finnhub.news_general (datetime DESC);
+CREATE UNIQUE INDEX idx_news_general_id ON finnhub.news_general (id);
+
+-- materialized view
+
+CREATE MATERIALIZED VIEW finnhub.news_general_view AS
+SELECT category,
+       to_timestamp(datetime) AT TIME ZONE 'UTC' AS datetime,
+       headline,
+       id,
+       image,
+       related,
+       source,
+       summary,
+       url
+FROM finnhub.news_general;
+
+-- materialized view indexes
+
+CREATE INDEX idx_news_general_view_datetime ON finnhub.news_general_view (datetime DESC);
---
+++ src/main/resources/db/migration/V5__news_crypto.sql
@@ -0,0 +1,42 @@
+-- Table: finnhub.news_crypto
+
+-- DROP TABLE IF EXISTS finnhub.news_crypto;
+
+CREATE TABLE IF NOT EXISTS finnhub.news_crypto
+(
+  category text COLLATE pg_catalog."default",
+  datetime integer,
+  headline text COLLATE pg_catalog."default",
+  id       integer,
+  image    text COLLATE pg_catalog."default",
+  related  text COLLATE pg_catalog."default",
+  source   text COLLATE pg_catalog."default",
+  summary  text COLLATE pg_catalog."default",
+  url      text COLLATE pg_catalog."default"
+) TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS finnhub.news_crypto
+  OWNER to vertx_demo_dev_user;
+
+-- indexes
+
+CREATE INDEX idx_news_crypto_datetime_desc ON finnhub.news_crypto (datetime DESC);
+CREATE UNIQUE INDEX idx_news_crypto_id ON finnhub.news_crypto (id);
+
+-- materialized view
+
+CREATE MATERIALIZED VIEW finnhub.news_crypto_view AS
+SELECT category,
+       to_timestamp(datetime) AT TIME ZONE 'UTC' AS datetime,
+       headline,
+       id,
+       image,
+       related,
+       source,
+       summary,
+       url
+FROM finnhub.news_crypto;
+
+-- materialized view indexes
+
+CREATE INDEX idx_news_crypto_view_datetime ON finnhub.news_crypto_view (datetime DESC);
