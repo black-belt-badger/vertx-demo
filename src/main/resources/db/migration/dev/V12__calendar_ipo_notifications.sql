02f17d11c136ab8f2ffa8333c84ff39ec47d9084
Marek Dudek <marek.dudek@gmail.com>
2025-07-31 18:54:19 +0200

    migration - next

---
+++ src/main/resources/db/migration/dev/V12__calendar_ipo_notifications.sql
@@ -0,0 +1,28 @@
+-- FUNCTION: public.notify_calendar_ipo_change()
+
+-- DROP FUNCTION IF EXISTS public.notify_calendar_ipo_change();
+
+CREATE OR REPLACE FUNCTION finnhub.notify_calendar_ipo_change()
+  RETURNS trigger
+  LANGUAGE 'plpgsql'
+  COST 100
+  VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+  PERFORM pg_notify('calendar_ipo_change_channel', TG_TABLE_NAME);
+  RETURN NEW;
+END;
+$BODY$;
+
+ALTER FUNCTION finnhub.notify_calendar_ipo_change()
+  OWNER TO vertx_demo_dev_user;
+
+-- Trigger: calendar_ipo_change
+
+-- DROP TRIGGER IF EXISTS calendar_ipo_change ON finnhub.calendar_ipo;
+
+CREATE OR REPLACE TRIGGER finnhub.calendar_ipo_change
+  AFTER INSERT OR DELETE OR UPDATE
+  ON finnhub.calendar_ipo
+  FOR EACH ROW
+EXECUTE FUNCTION public.notify_calendar_ipo_change();
