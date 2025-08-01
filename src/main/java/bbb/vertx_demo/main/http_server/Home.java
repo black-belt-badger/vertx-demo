bf22c3988c8a544dcff83e60eea8c0f97fb49be3
Marek Dudek <marek.dudek@gmail.com>
2025-08-01 13:45:12 +0200

    home page

--- src/main/java/bbb/vertx_demo/main/http_server/Home.java
+++ src/main/java/bbb/vertx_demo/main/http_server/Home.java
@@ -64,28 +64,26 @@ public enum Home {
                 .end(buffer);
               log.info("Home page request handled in {}", watch.elapsed());
             } else {
-              var query = """
+              var iposQuery = """
                 SELECT *
-                FROM (
-                       SELECT *,
-                              ROW_NUMBER() OVER (PARTITION BY exchange ORDER BY date DESC) AS rn
-                       FROM finnhub.calendar_ipo_parsed
-                     ) sub
-                WHERE rn <= 3;""";
+                              FROM finnhub.calendar_ipo_parsed
+                              WHERE date >= CURRENT_DATE
+                              ORDER BY date
+                """;
               pgConnection
-                .query(query)
+                .query(iposQuery)
                 .execute()
                 .onFailure(throwable -> {
-                    checks.register(query, promise ->
+                    checks.register(iposQuery, promise ->
                       promise.complete(KO(), throwable)
                     );
-                    log.error("Query {} failed", query, throwable);
+                    log.error("Query {} failed", iposQuery, throwable);
                   }
                 )
-                .onSuccess(rowSet -> {
+                .onSuccess(iposRowSet -> {
                     var renderingContext = new HashMap<String, Object>();
                     renderingContext.put("pageTitle", "Home page");
-                    var elements = rowSet.stream().map(row -> {
+                    var ipoElements = iposRowSet.stream().map(row -> {
                         var element = new HashMap<String, Object>();
                         element.put("date", row.getLocalDate("date"));
                         element.put("name", row.getString("name"));
@@ -101,25 +99,62 @@ public enum Home {
                       }
                     ).toList();
                     var elementsByExchange =
-                      elements.stream().collect(groupingBy(map ->
+                      ipoElements.stream().collect(groupingBy(map ->
                         map.getOrDefault("exchange", "null").toString())
                       );
                     renderingContext.put("elementsByExchange", elementsByExchange);
-                    engine
-                      .render(renderingContext, "templates/index.html")
-                      .onFailure(throwable ->
-                        log.error("error rendering '{}' template", name, throwable)
+                    var newsQuery = """
+                      SELECT TO_CHAR(datetime, 'HH24:MI') AS time, headline, summary, url
+                         FROM finnhub.news_general_view
+                         WHERE datetime >= CURRENT_DATE
+                         UNION ALL
+                         SELECT TO_CHAR(datetime, 'HH24:MI') AS time, headline, summary, url
+                         FROM finnhub.news_crypto_view
+                         WHERE datetime >= CURRENT_DATE
+                         UNION ALL
+                         SELECT TO_CHAR(datetime, 'HH24:MI') AS time, headline, summary, url
+                         FROM finnhub.news_merger_view
+                         WHERE datetime >= CURRENT_DATE
+                         ORDER BY time DESC
+                      """;
+                    pgConnection
+                      .query(newsQuery)
+                      .execute()
+                      .onFailure(throwable -> {
+                          checks.register(newsQuery, promise ->
+                            promise.complete(KO(), throwable)
+                          );
+                          log.error("Query {} failed", newsQuery, throwable);
+                        }
                       )
-                      .onSuccess(buffer -> {
-                          context.response()
-                            .putHeader(CONTENT_TYPE, HTML)
-                            .putHeader(CACHE_CONTROL, cacheControl)
-                            .end(buffer);
-                          var request = cmd(SETEX)
-                            .arg(REDIS_KEY)
-                            .arg(maxAge)
-                            .arg(buffer);
-                          redisConnection.send(request);
+                      .onSuccess(newsRowSet -> {
+                          var newsElements = newsRowSet.stream().map(row -> {
+                              var element = new HashMap<String, Object>();
+                              element.put("time", row.getString("time"));
+                              element.put("headline", String.valueOf(row.getString("headline")));
+                              element.put("summary", String.valueOf(row.getString("summary")));
+                              element.put("url", String.valueOf(row.getString("url")));
+                              return element;
+                            }
+                          ).toList();
+                          renderingContext.put("news", newsElements);
+                          engine
+                            .render(renderingContext, "templates/index.html")
+                            .onFailure(throwable ->
+                              log.error("error rendering '{}' template", name, throwable)
+                            )
+                            .onSuccess(buffer -> {
+                                context.response()
+                                  .putHeader(CONTENT_TYPE, HTML)
+                                  .putHeader(CACHE_CONTROL, cacheControl)
+                                  .end(buffer);
+                                var request = cmd(SETEX)
+                                  .arg(REDIS_KEY)
+                                  .arg(maxAge)
+                                  .arg(buffer);
+                                redisConnection.send(request);
+                              }
+                            );
                         }
                       );
                   }
